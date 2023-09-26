dda74b17b4d2a255685decf465a9338edcfdef05
Cody De Arkland <codydearkland@gmail.com>
2023-09-26 16:18:23 -0700

    adding updated things to the ui

--- components/additem.tsx
+++ components/additem.tsx
@@ -7,26 +7,44 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import { UploadDropzone } from "@uploadthing/react";
 import { OurFileRouter } from "../app/api/uploadthing/core";
 import { Plus } from "lucide-react";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type InventoryItem = {
-    id: number;
-    name: string;
-    brand: string;
-    imageurl: string;
-    user_id: string;
-  };
+  id: number;
+  name: string;
+  brand: string;
+  imageurl: string;
+  user_id: string;
+};
 
-export const AddItem = ({ session, inventory, setInventory}: {session: any, inventory: InventoryItem[], setInventory: React.Dispatch<React.SetStateAction<InventoryItem[]>>}) => {
+export const AddItem = ({
+  session,
+  inventory,
+  setInventory,
+}: {
+  session: any;
+  inventory: InventoryItem[];
+  setInventory: React.Dispatch<React.SetStateAction<InventoryItem[]>>;
+}) => {
   const [uploadURL, setUploadURL] = useState<string>("");
   const [open, setOpen] = useState(false);
   const [uploaded, setUploaded] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<string>("");
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -43,7 +61,7 @@ export const AddItem = ({ session, inventory, setInventory}: {session: any, inve
     const data = await response.json();
     console.log(data[0]);
     await setInventory([...inventory, data[0]]);
-    setOpen(false);    
+    setOpen(false);
   }
 
   async function settheurl(res: any) {
@@ -51,6 +69,10 @@ export const AddItem = ({ session, inventory, setInventory}: {session: any, inve
     await setUploaded(true);
     console.log(uploadURL);
   }
+
+  useEffect(() => {
+    console.log(selectedItem);
+  }, [selectedItem]);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -68,12 +90,44 @@ export const AddItem = ({ session, inventory, setInventory}: {session: any, inve
           </DialogDescription>
         </DialogHeader>
         <form onSubmit={(e) => handleSubmit(e)}>
-        <div className="grid items-center">
+          <div className="grid items-center">
+            <div className="mx-auto">
+              <div className="flex mb-4 justify-center">
+                <Label className="text-xl">Whats kind of thing?</Label>
+              </div>
+              <Select onValueChange={(e) => setSelectedItem(e)}>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Your Thing" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="generic">Generic Thing</SelectItem>
+                  <SelectItem
+                    value="seasoning"
+                    onClick={() => setSelectedItem("seasoning")}
+                  >
+                    Seasoning
+                  </SelectItem>
+                  <SelectItem
+                    value="storage"
+                    onClick={() => setSelectedItem("storage")}
+                  >
+                    Storage Bin
+                  </SelectItem>
+                  <SelectItem
+                    value="toy"
+                    onClick={() => setSelectedItem("toy")}
+                  >
+                    Toy
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
             {!uploaded ? (
               <UploadDropzone<OurFileRouter>
                 endpoint="imageUploader"
-                className="hover:border-2 hover:border-white mb-4"
-                config={{ mode: "auto"}}
+                className=""
+                config={{ mode: "auto" }}
                 onClientUploadComplete={(res) => {
                   settheurl(res);
                 }}
@@ -82,17 +136,61 @@ export const AddItem = ({ session, inventory, setInventory}: {session: any, inve
                 }}
               />
             ) : (
-              <div><img src={uploadURL} className="max-w-xs max-h-64 lg:max-w-md pb-4 mx-auto"/></div>
+              <div>
+                <img
+                  src={uploadURL}
+                  className="max-w-xs max-h-64 lg:max-w-md pb-4 mx-auto"
+                />
+              </div>
             )}
           </div>
-          <div className="flex items-center justify-between pb-4">
+
+          {selectedItem === "generic" && (
+            <div className="pb-4">
+              <div className="pb-4">
+                {/* <Label className="pr-2">Name</Label> */}
+                <Input placeholder="Name" id="name" />
+              </div>
+              <div>
+                {/* <Label className="pr-2">Brand</Label> */}
+                <Input placeholder="Brand" id="brand" />
+              </div>
+            </div>
+          )}
+          {selectedItem === "seasoning" && (
+            <div className="">
+              <div className="pb-4">
+                {/* <Label className="pr-2 mx-auto">Seasoning Name</Label> */}
+                <Input placeholder="Seasoning Name" id="name" />
+              </div>
+              <div>
+                {/* <Label className="pr-2">Brand</Label> */}
+                <Input placeholder="Seasoning Brand" id="brand" />
+              </div>
+            </div>
+          )}
+          {selectedItem === "storage" && (
+            <div className="space-y-2 pb-4">
+              {/* <Label className="pr-2"></Label> */}
+              <Input placeholder="Contents" id="name" />
+              <Input placeholder="Type" id="brand" /> 
+            </div>
+          )}
+          {selectedItem === "toy" && (
+            <div>
+              {/* <Label className="pr-2">Toy Brand</Label> */}
+              <Input placeholder="Toy Brand" id="name" />
+            </div>
+          )}
+
+          {/* <div className="flex items-center justify-between pb-4">
             <Label className="pr-2">Name</Label>
             <Input placeholder="Name" id="name" />
           </div>
           <div className="flex items-center pb-4">
             <Label className="pr-2">Brand</Label>
             <Input placeholder="Brand" id="brand" />
-          </div>
+          </div> */}
           <Button variant={"outline"} type="submit" className="flex mx-auto">
             Submit
           </Button>
